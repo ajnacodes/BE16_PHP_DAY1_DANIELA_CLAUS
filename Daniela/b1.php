b483056723c72e199e70363f6193cad741de45ac
ajnacodes <b.daniela196@outlook.com>
2022-07-11 12:32:02 +0200

    basic exercise one

---
+++ Daniela/b1.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<head>
+ 
+<link rel="stylesheet" href="styles.css">
+    <title>Basic Exercise 1</title>
+</head>
+
+<body>
+    
+
+
+<?php 
+
+$name = "Daniela";
+
+echo "
+
+<div class='b1nameBox'>
+
+<div class='b1name'>
+
+<h1>$name</h1>
+
+</div>
+
+</div>
+
+";
+
+?>
+
+
+
+
+</body>
+
+</html>
