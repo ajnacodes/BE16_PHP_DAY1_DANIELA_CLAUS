8f70a9e67fc2ab519e752bcfe713c9c4dc480979
OinkoIT <claus.mersch@gmail.com>
2022-07-11 11:15:05 +0200

    classwork 1 & 2

---
+++ hello-world.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="UTF-8">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <title>Classwork</title>
+       <link rel="stylesheet" href="style.css">
+   </head>
+   <body>
+
+<?php
+$name = 'Claus';
+$surName = 'Mersch';
+$fullName;
+$age = 33;
+$jobTitle = "Fachreferent";
+//variables
+
+function printName()
+{  
+   $GLOBALS['fullName'] = $GLOBALS['name'].' '.$GLOBALS['surName'];
+   return $GLOBALS['fullName'];
+}
+echo "<h1 class='middle'>" . printName() . "</h1>";
+
+echo "<h1> Hello, my name is " . printName() . " and I am {$age} years old. I work as a {$jobTitle}." . "</h1>";
+
+// echo $fullName;
+
+// $array = [1,2,3,4,5,6,7,8,9];
+// echo count($array);
+
+/*echo '<br>';//it insert a line break 
+
+var_dump( $GLOBALS );*/ ?>
+   </body>
+</html>
