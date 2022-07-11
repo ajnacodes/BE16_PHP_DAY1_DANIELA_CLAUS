e74ef37ac533154dc85a56828e469a4d4e01bc9c
OinkoIT <claus.mersch@gmail.com>
2022-07-11 13:20:43 +0200

    challenge

--- hello-world.php
+++ hello-world.php
@@ -30,9 +30,21 @@ echo "<h1> Hello, my name is " . printName() . " and I am {$age} years old. I wo
 
 // CLASSWORK 3
 
-$players = ["Mark", "John", "Georg", "Lisa"];
+// $players = ["Mark", "John", "Georg", "Lisa"];
 
-echo "<h4>The third player in the team is {$players[2]}<h4>";
+// echo "<h4>The third player in the team is {$players[2]}<h4>";
+
+$players = ["name" => [
+   "1" => "Mark",
+   "2" => "John",
+   "3" => "Georg",
+   "4" => "Lisa"]
+];
+echo "
+<h3>
+The third player in the team is {$players["name"]["3"]}.
+</h3>
+";
 
 // ADVANCED 1
 
@@ -46,13 +58,48 @@ $composer = array(
 
 foreach($composer as $arrKey=>$arrVal) {echo "<p>The song {$arrKey} is composed by {$arrVal}<p>";};
 
-// echo $fullName;
+/* This chapter will be Project-based. We will create a Car rental agency. 
 
-// $array = [1,2,3,4,5,6,7,8,9];
-// echo count($array);
+The Car agency should show the availability of the cars, the make, and models of the cars, price, and location of the specific rental place.
+
+The second thing you need to do is to create user-friendly GUI rendering. You should use Bootstrap elements to create GUI for this project. Come up with an idea of which data you would like to present and which pages (forms, tables) you need in your GUI part and create them.  */
 
 /*echo '<br>';//it insert a line break 
 
-var_dump( $GLOBALS );*/ ?>
+var_dump( $GLOBALS );*/ 
+
+$cars = [
+   "vw" => [
+   "model" => "Golf",
+   "built" => 2020,
+   "price" => 80,
+   "location" => "Vienna",
+   "img" => "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8a/2020_Volkswagen_Golf_Style_1.5_Front.jpg/2880px-2020_Volkswagen_Golf_Style_1.5_Front.jpg"
+   ],
+   "skoda" => [
+      "model" => "Octavia",
+      "built" => 2021,
+      "price" => 60,
+      "location" => "Graz",
+      "img" => "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Skoda_Octavia_IV_Combi_IMG_2795.jpg/2560px-Skoda_Octavia_IV_Combi_IMG_2795.jpg"
+   ],
+   "bmw" => [
+      "model" => "3er",
+      "built" => 2018,
+      "price" => 95,
+      "location" => "Salzburg",
+      "img" => "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/2004_BMW_320_CD_Sport_2.0.jpg/2560px-2004_BMW_320_CD_Sport_2.0.jpg"
+   ], 
+   "tesla" => [
+      "model" => "Model 3",
+      "built" => 2022,
+      "price" => 45,
+      "location" => "Linz",
+      "img" => "https://upload.wikimedia.org/wikipedia/commons/8/83/Tesla_Model_3_parked%2C_front_driver_side.jpg"
+   ]  
+];
+
+
+?>
    </body>
 </html>
